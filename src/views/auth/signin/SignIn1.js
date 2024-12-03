ee7a169b2bce05aeac80c7499e5cfb2fb4a3321e
Muh. Anshar Azhari <muh.ansharazhari@gmail.com>
2024-12-03 22:37:14 +0700

    fix register

--- src/views/auth/signin/SignIn1.js
+++ src/views/auth/signin/SignIn1.js
@@ -34,7 +34,7 @@ const Signin1 = () => {
               </p>
               <p className="mb-0 text-muted">
                 Don’t have an account?{' '}
-                <NavLink to="/auth/signup-1" className="f-w-400">
+                <NavLink to="/register" className="f-w-400">
                   Signup
                 </NavLink>
               </p>
